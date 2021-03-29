2ad560ff2098cf15069138d556cf4c4a5a2e6ed0
chengang <aadyx2007@163.com>
2021-03-29 17:50:25 +0800

    完善功能
    Signed-off-by: chengang <aadyx2007@163.com>

--- MKLoRaTracker/Classes/CTMediator/MKLoRaWANLTModuleKey.h
+++ MKLoRaTracker/Classes/CTMediator/MKLoRaWANLTModuleKey.h
@@ -6,4 +6,4 @@ NSString *const kAction_loRaApp_lt_aboutPage = @"LoRaWANLT_Module_AboutControlle
 
 //壳工程关于页面
 NSString *const kTarget_loRaApp_la_module = @"LoRaWANLA_Module";
-NSString *const kAction_loRaApp_la_aboutPage = @"LoRaWANLA_LB_AboutController";
+NSString *const kAction_loRaApp_la_aboutPage = @"LoRaWANLA_LT_AboutController";
